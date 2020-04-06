c7f35a1a4bb25051dcbec3a825db55e1b9a22051
Chris Perks <chris.perks@drawgroup.com>
2020-04-06 12:42:35 +0100

    Add comments

--- kubeless-starting/assets/lower.py
+++ kubeless-starting/assets/lower.py
@@ -1,6 +1,10 @@
+# This is a standard python file
+# We can use this file for our Kubeless function
+
 def handler(event, context):
    print event
    return lowercase(event['data'])
 
+# A silly function to apply lower() to each value in a dictionary
 def lowercase(dict): 
    return {k:v.lower() for k, v in dict.items()}
--- kubeless-starting/assets/upper.py
+++ kubeless-starting/assets/upper.py
@@ -1,6 +1,10 @@
+# This is a standard python file
+# We can use this file for our Kubeless function
+
 def handler(event, context):
    print event
    return uppercase(event['data'])
 
+# A silly function to apply upper() to each value in a dictionary
 def uppercase(dict): 
    return {k:v.upper() for k, v in dict.items()}
